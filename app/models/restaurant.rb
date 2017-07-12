9673cf0d1be52ebbd9a583cb0d911a0deb241dd3
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-12 13:21:53 +0200

    modified data type for phone number in restaurants table

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
+  validates_associated :reviews
 end

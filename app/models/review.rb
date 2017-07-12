cb45a78d47e3272def8204259a9a02021903b7fa
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-12 13:27:32 +0200

    validated restaurant and review model with rake

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }
+  validates :rating, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }, numericality: [only_integer: true]
   validates :content, presence: true
-  validates_associated :restaurant
 end

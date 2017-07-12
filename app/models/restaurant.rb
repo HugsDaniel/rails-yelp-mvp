4e1046f7c314611048cb800cbc7114a32fdd44fb
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-12 13:07:59 +0200

    added methods to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "italian",
+    phone_number: 0296856548
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "chinese",
+    phone_number: 0679584525
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "french",
+    phone_number: 0745859565
+  },
+  {
+    name:         "Chez Jean-Mich",
+    address:      "Place de l'Eglise 22800 Boquého",
+    category:     "belgian",
+    phone_number: 0648495948
+  },
+  {
+    name:         "Le St-Do",
+    address:      "Place de la Mairie 22800 Saint-Donan",
+    category:     "french",
+    phone_number: 0296739594
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'

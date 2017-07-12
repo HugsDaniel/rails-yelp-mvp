d47b2053c48f1992083f754a8584843c1cd74c49
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-12 15:11:30 +0200

    rake passed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  # before_action :authorize, only: [:new, :create, :edit, :update, :destroy]
   def index
     @restaurants = Restaurant.all
   end
@@ -14,26 +15,32 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.create(restaurant_params)
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-    # @restaurant.update(restaurant_params)
-    # redirect_to restaurant_path(@restaurant)
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
   private
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :category, :phone_number)
-    end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'restaurants#index'
   resources :restaurants do
-    resources :reviews, only: [ :create ]
+    resources :reviews, only: [ :create, :new ]
   end
 
   # These routes will be for signup. The first renders a form in the browse, the second will
   # receive the form and create a user in our database using the data given to us by the user.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 end
